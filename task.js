8b0da8f59e3d9562b01b8cfcb67b29a5705c5764
uNaple <retkonstantin@gmail.com>
2016-06-23 17:56:56 +0300

    Проверка корректности

--- task.js
+++ task.js
@@ -1,5 +1,6 @@
 //Описание задачи и ее свойств, проверка их корректности про добавлении и редактировании
-var db = require('./modules/db');
+var db 		= require('./modules/db'),
+	Promise = require('bluebird');
 var statusArray  = ["В процессе", "Закончена", "Приостановлена", "Добавлена/Ожидает принятия", "Ожидает завершения подзадачи", "Отменена"],
 	typeOfAction = ["Добавлена", "Переназначена", "Статус сменен на *"],
 	typeArray    = ['Проект','Задача','Подзадача'];
@@ -31,28 +32,61 @@ function myTask() {           										//объект Задание
 	this.repeat = null;												//когда повторять, например задача отправить ЗП на определенное число месяца
 }
 
-myTask.prototype.checkParent = function(){							//проверка правильности родителя
+myTask.prototype.checkType = function(){							//проверка правильности родителя у заданного типа задачи, расширить еще
 	var self = this;
-	//console.log(self.type);
-	//console.log(self.parent);
-	if(self.type == typeArray[2] && self.parent == null){
-		console.log('У подзадачи должен быть родитель. Задача: ' + self.name);
-		return null;
-	}
-	else 
-		return true;
+	return new Promise(function(resolve, reject){
+		if(self.type == typeArray[2] && self.parent == null){		//Если подзадача, то должен быть родитель
+			console.log('У подзадачи должен быть родитель. Задача: ' + self.name);
+			reject();
+			//return null;
+		}
+		else 
+			resolve();
+			//return true;
+	})
 }
 
+// myTask.prototype.checkParent = function(list){						//Если есть родитель, то он должен быть из списка задач
+// 	var self = this;
+// 	return new Promise(function(resolve, reject){
+// 		db.loadAll(function(err, res){
+// 			if(err)
+// 				reject(err);
+// 			else
+// 				resolve(res);
+// 		})
+// 	}).then(function(res){
+// 		//console.log(res);
+// 		for(var i = 0; i < res.length; i++){
+// 			console.log(self.parent + ' ' + res[i].name);
+// 			if(self.parent == res[i].name){
+// 				console.log('1');
+// 				return true;
+// 				break;
+// 			}															//Прошел по массиву не нашел родительской задачи в списке
+// 		}
+// 		var err1 = new Error('Родитель задачи был удален или отсутствует');
+// 		return false;
+// 	}, function(){
+// 		console.log('2 ');
+// 		return false;
+// 	}
+// 	// return null;
+// 	// console.log('Родитель задачи был удален или отсутствует');
+// }
+
 myTask.prototype.checkThis = function(){							//тут собрать вместе все проверки на дату, на родителя, и пускать задачу дальше только если все ок
-	if(this.checkParent())
+	console.log(this.checkParent());
+	if(this.checkType() && this.checkParent()){
 		return true;
+	}
 	else {
 		console.log('check this find error ');
 		return null;
 	}
 }
 
-myTask.prototype.init = function(obj){								//заполняю задачу из объекта
+myTask.prototype.init = function(obj){								//заполняю задачу из объекта для дальнейшей проверки
 	for(var i in obj){
 		this[i] = obj[i];
 	}
